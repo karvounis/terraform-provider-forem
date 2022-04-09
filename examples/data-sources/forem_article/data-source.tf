3d55330216e3aadfe04eda0a294b44637a6c2a07
karv <karvounis@users.noreply.github.com>
2022-04-09 18:54:24 +0300

    add data source forem_article functionality by username and slug

--- examples/data-sources/forem_article/data-source.tf
+++ examples/data-sources/forem_article/data-source.tf
@@ -1,3 +1,8 @@
-data "forem_article" "example" {
+data "forem_article" "example_id" {
   id = "979788"
+}
+
+data "forem_article" "example_username_slug" {
+  username = "admin_mcadmin"
+  slug     = "basic-traefik-configuration-tutorial-593m"
 }
